c96f0146ef5e50741ca31e5e1c3b1baa46147dd9
treemoss1233 <125329855+treemoss1233@users.noreply.github.com>
2023-11-07 08:16:09 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
-from kerykeion import AstrologicalSubject
+#from kerykeion import AstrologicalSubject
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    person = AstrologicalSubject("Person", 1977, 6, 8, 8, 45, "Atlanta")
+    #person = AstrologicalSubject("Person", 1977, 6, 8, 8, 45, "Atlanta")
 
     #Get the information about the person
-    astroInfo = person.sun
+    #astroInfo = person.sun
     
-    return "Sun Sign: " + astroInfo
+    return "Sun Sign: " #+ astroInfo
