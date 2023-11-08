ac2fd8278b6579830d34793a55282b94f13f6866
treemoss1233 <125329855+treemoss1233@users.noreply.github.com>
2023-11-08 12:32:41 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from kerykeion import AstrologicalSubject
+from openai import OpenAI
+openai.api_key = "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"
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    from openai import OpenAI
+    
     client = OpenAI()
     
     empty_thread = openai.beta.threads.create()
